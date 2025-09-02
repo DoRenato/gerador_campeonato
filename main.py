93e308560e6fe0a1141572cbd38539081057f99a
DoRenato <64191958+DoRenato@users.noreply.github.com>
2025-09-02 18:14:57 -0300

    Att main.py e exemplos
    - Arquivo main.py atualizado para receber o parametro qtd_grupos;
    
    - Adicionado exemplos de lista de times e saidas para todos os formatos de campeonatos

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ campeonato=criar_campeonato(
     times_json='times', # Apenas o nome do arquivo, sem extensão. O programa já reconhece que será um json
     randozimar_times=False, # Se True, vai randomizar os times que foi escrito no json
     casa_fora=True, # Se True, irá criar as rodadas de casa e fora
-    tipo_campeonato=1 # 1= Pontos corridos, 2= Fase de grupos
+    tipo_campeonato=1, # 1= Pontos corridos, 2= Fase de grupos
+    # qtd_grupos=2 # Parâmetro opcional, usado somente no formato fase de grupos, para definir a quantidade de grupos.
 )
